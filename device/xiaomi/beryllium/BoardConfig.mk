37c6ccffcc341617d7416944d06d6569e992d1a1
Misc <miscwork2062@gmail.com>
2022-08-25 19:09:00 +0530

    AOSP - Files

---
+++ device/xiaomi/beryllium/BoardConfig.mk
@@ -0,0 +1,25 @@
+#
+# Copyright (C) 2018 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from sdm845-common
+-include device/xiaomi/sdm845-common/BoardConfigCommon.mk
+
+DEVICE_PATH := device/xiaomi/beryllium
+
+# Assert
+TARGET_OTA_ASSERT_DEVICE := beryllium
+
+# Crypto
+TARGET_HW_DISK_ENCRYPTION := true
+
+# Kernel
+TARGET_KERNEL_CONFIG := beryllium_defconfig
+
+# Inherit from the proprietary version
+-include vendor/xiaomi/beryllium/BoardConfigVendor.mk
+
+# Sepolicy
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
